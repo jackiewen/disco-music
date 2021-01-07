187f98e9c09fd5a01dc353dc9d90fea3738a1d4c
Van Qui <van.qui.rcvn2012@gmail.com>
2021-01-07 16:59:41 +0700

    layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,15 @@ import classes from './Home.module.scss';
 
 const Home = () => {
     return (
+        <>
         <div className={classes.SliderBackground}>
             <SliderBar items={[]} />
-            <div className="container">
-                <h1>This is home page</h1>
-            </div>
+            
         </div>
-        
+        <div className="container">
+            <h1>This is home page</h1>
+        </div>
+        </>
     );
 }
 
